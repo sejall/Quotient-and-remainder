3482265779b02ce863af6825f43e00ecb742c8b6
sejall <33982474+sejall@users.noreply.github.com>
2018-09-22 06:26:39 +0530

    Add files via upload

---
+++ Python Program to Read Two Numbers and Print Their Quotient and Remainder.py
@@ -0,0 +1,9 @@
+
+#Python Program to Read Two Numbers and Print Their Quotient and Remainder
+
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+quotient=a//b
+remainder=a%b
+print("Quotient is:",quotient)
+print("Remainder is:",remainder)
